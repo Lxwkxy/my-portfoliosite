ffbf046d0891b5bda56f9b6383e69624b34cc88c
Lxwkxy <paingaikrab1234@gmail.com>
2024-10-16 21:09:01 +0700

    Update InfoThree.jsx

--- src/components/InfoThree.jsx
+++ src/components/InfoThree.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 function InfoThree() {
   return (
     <div className='bg-gray-200'>
-      <div className='container mx-auto flex item-center py-16'>
-      <div className="w-1/2 flex justify-center">
+      <div className='container mx-auto flex item-center py-16 flex flex-col md:flex-row'>
+      <div className="w-full md:full">
         <img src="https://github.com/Lxwkxy/my-portfoliosite/blob/main/img/transcript_front.jpg?raw=true" alt="transcipt_front" className='w-auto h-auto p-10'/>
       </div>
-      <div className="w-1/2 flex justify-center">
+      <div className="w-full md:full">
         <img src="https://github.com/Lxwkxy/my-portfoliosite/blob/main/img/transcript_back.jpg?raw=true" alt="transcipt_back" className='w-auto h-auto p-10'/>
       </div>
     </div>
-    <div className="w-1/2 px-6">
+    <div className="w-full md:w-1/3 px-6">
           <div className="w-full px-4 py-10 mb-8">
           <div className="bg-white p-6 rounded-lg shadow-md">
           <div className='bg-blue-500 text-white px-6 py-2 rounded-full inline-block font-thaiFont'>
