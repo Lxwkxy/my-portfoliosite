457a386af622357920f0a509e8bf4fbc9ee1b721
Lxwkxy <paingaikrab1234@gmail.com>
2024-10-16 21:08:52 +0700

    Update InfoOne.jsx

--- src/components/InfoOne.jsx
+++ src/components/InfoOne.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function InfoOne() {
   return (
     <div className='bg-gray-200'>
-    <div className='container mx-auto flex item-center py-16'>
-      <div className="w-1/2 md:w-1/3 lg:w-1/4">
+    <div className='container mx-auto flex item-center py-16 flex flex-col md:flex-row'>
+      <div className="md:w-3/4 lg:w-1/2 justify-items-center">
         <img src="https://github.com/Lxwkxy/my-portfoliosite/blob/main/img/me_img.jpg?raw=true" alt="Me" className='w-full h-auto rounded-full p-10'/>
       </div>
-      <div className="w-1/2 px-6">
-          <div className='bg-blue-500 text-white px-6 py-2 rounded-full mt-8 inline-block font-thaiFont'>
+      <div className="md:w-3/4 lg:w-1/2 px-6 justify-items-center">
+          <div className='bg-blue-500 text-white px-6 py-2 rounded-full mt-8 inline-block font-thaiFont justify-self-center'>
             ประวัติส่วนตัว
           </div>
           <div className="w-full px-4 py-10 mb-8">
