757312585b6a18700ad044372c63ae91567e1093
Lxwkxy <paingaikrab1234@gmail.com>
2024-10-16 21:16:53 +0700

    Update InfoTwo.jsx

--- src/components/InfoTwo.jsx
+++ src/components/InfoTwo.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 function InfoTwo() {
   return (
-    <div className='container size-3/4 mx-auto flex item-center py-16'>
-      <div className="grid grid-cols-3 gap-4 flex flex-wrap justify-center mt-8 ">
+    <div className='container mx-auto flex item-center py-16'>
           <div className="p-5 grid justify-items-center">
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png" alt="HTML5" className='w-full md:w-3/4 p-5'/>
             <img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" alt="CSS3" className='w-full md:w-3/4 p-5'/>
@@ -18,7 +17,6 @@ function InfoTwo() {
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2560px-Tailwind_CSS_Logo.svg.png" alt="Tailwind CSS" className='w-full md:w-3/4 p-5'/>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/1024px-Visual_Studio_Code_1.35_icon.svg.png" alt="Visual Studio Code" className='w-full md:w-3/4 p-5'/>
           </div>
-      </div>
     </div>
   )
 }
