089881709c65f9bc5a6433b056d03da7adc84e1c
Lxwkxy <paingaikrab1234@gmail.com>
2024-10-16 21:08:23 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='font-serif bg-gray-900 text-white py-16'>
+    <div className='font-serif bg-gray-900 text-white py-16 text-center sm:text-left'>
         <div className="container mx-auto text-center">
             <h1 className='text-5xl font-extrabold'>Welcome to my portfolio website.</h1>
             <p className='text-lg mt-4'>Want to know more about me?</p>
